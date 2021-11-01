99cddea16802f0bb0e7c641059b6cbac665ed799
uGR17 <u.guadarrama@hotmail.com>
2021-11-01 11:45:50 +0100

    v0.0.7
    all packages work

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["dataparser>=0.0.1",\
+requirements = ["dateparser>=0.0.1",\
     "torch>=1.7.0",\
     "prophet<2.0.0,>=1.0.1",\
     "colorhash<2.0.0,>=1.0.3",\
@@ -18,7 +18,7 @@ requirements = ["dataparser>=0.0.1",\
 
 setup(
     name="timexseries_c",
-    version="0.0.6",
+    version="0.0.7",
     author="Uriel Guadarrama Ramirez",
     author_email="u.guadarrama@hotmail.com",
     description="TIMEX-CLUSTERING is a framework for time-series-clustering-as-a-service",
