2d8c13b447f1d70a4bf7535ec7543d4486c9de44
uGR17 <u.guadarrama@hotmail.com>
2021-11-17 12:22:29 +0100

    Update timeseries_container.py

--- timexseries_clustering/timeseries_container.py
+++ timexseries_clustering/timeseries_container.py
@@ -14,17 +14,17 @@ class TimeSeriesContainer:
         Dictionary of ModelResult objects, all trained on these time-series.
     xcorr : dict
         Cross-correlation between the data of this time-series and all the other ones.
-    historical_prediction : dict
+    centroids : dict
         The historical prediction, i.e. the predictions computed on a rolling window on the historical data.
         This is useful to verify the performances of each model not only on the very last data, but throughout the
         history of the time-series, in a cross-validation fashion. This dictionary contains one entry for each model
         tested.
     """
-    def __init__(self, timeseries_data: DataFrame, models: dict, xcorr: dict, historical_prediction: dict = None):
+    def __init__(self, timeseries_data: DataFrame, models: dict, xcorr: dict, centroids: dict = None):
         self.timeseries_data = timeseries_data
         self.models = models
         self.xcorr = xcorr
-        self.historical_prediction = historical_prediction
+        self.centroids = centroids
 
-    def set_historical_prediction(self, historical_prediction):
-        self.historical_prediction = historical_prediction
+    def set_centroids(self, centroids):
+        self.centroids = centroids
