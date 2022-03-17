90e7ac74de9024b5e4499a668815391c7d345f91
uGR17 <u.guadarrama@hotmail.com>
2022-03-17 15:43:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = ["dateparser<2.0.0,>=1.0.0",\
 
 setup(
     name="timexseries_clustering",
-    version="1.0.12",
+    version="1.0.15",
     author="Uriel Guadarrama Ramirez",
     author_email="u.guadarrama@hotmail.com",
     description="TIMEX-CLUSTERING is a framework for time-series-clustering-as-a-service",
